d3f7abf2c936d8640c35011192e76e2909476dc5
Salustiano <salustianomeliann@gmail.com>
2025-05-21 17:46:06 -0300

    Se termino el ejercicio 2 y se comento tanto el ejercicio 1 como el indroductorio

--- Ejercicios/Ejercicio1/Ejercicio1.cs
+++ Ejercicios/Ejercicio1/Ejercicio1.cs
@@ -1,10 +1,10 @@
 //Verificar si el texto ingresado sea un numero y luego invertirlo
 
-Console.WriteLine("Ingrese el numero que quiere invertir");
+/*Console.WriteLine("Ingrese el numero que quiere invertir");
 int X = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine(X);
 while (X > 0)
 {
     Console.Write(X % 10);
     X = X / 10;
-}
+}*/
