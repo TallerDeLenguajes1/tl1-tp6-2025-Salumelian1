1307403a5a6d32426755ae4079207dc006ea66f4
Salumelian1 <salustianomeliann@gmail.com>
2025-06-26 20:06:50 -0300

    Se termino la Branch CalculadoraV2

--- Ejercicio3/Program.cs
+++ Ejercicio3/Program.cs
@@ -1,4 +1,6 @@
-﻿int Opcion = 0, num1 = 0, num2 = 0;
+﻿using System;
+int Opcion = 0, num1 = 0, num2 = 0; 
+double num = 0;
 
 Console.WriteLine("----------Incio del programa---------");
 do
@@ -7,10 +9,12 @@ do
     Console.WriteLine("2. Resta");
     Console.WriteLine("3. Producto");
     Console.WriteLine("4. Cociente");
+    Console.WriteLine("5. Operaciones con un solo número");
+    Console.WriteLine("6. Máximo y mínimo entre dos números");
     Console.WriteLine("0. Salir");
     Console.WriteLine("Ingrese la opcion que desea: ");
     Opcion = Convert.ToInt32(Console.ReadLine());
-    if (Opcion != 0)
+    if (Opcion >= 1 && Opcion <= 4 || Opcion == 6)
     {
             Console.WriteLine("Ingrese el primer numero: ");
             num1 = Convert.ToInt32(Console.ReadLine());
@@ -37,6 +41,21 @@ do
             {
                 Console.WriteLine("No se puede dividir en 0");
             }
+            break;
+        case 5:
+            Console.WriteLine("Ingrese un número: ");
+            num = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Valor absoluto: " + Math.Abs(num));
+            Console.WriteLine("Cuadrado: " + Math.Pow(num, 2));
+            Console.WriteLine("Raíz cuadrada: " + Math.Sqrt(num));
+            Console.WriteLine("Seno: " + Math.Sin(num));
+            Console.WriteLine("Coseno: " + Math.Cos(num));
+            Console.WriteLine("Parte entera: " + Math.Truncate(num));
+
+            break;
+        case 6: 
+            Console.WriteLine("Máximo: " + Math.Max(num1, num2));
+            Console.WriteLine("Mínimo: " + Math.Min(num1, num2));
             break;
     }
 } while (Opcion != 0);
