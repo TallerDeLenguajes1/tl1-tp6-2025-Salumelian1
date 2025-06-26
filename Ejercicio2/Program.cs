48b889642fea755fad227bf3cb003aa0577ffb03
Salumelian1 <salustianomeliann@gmail.com>
2025-06-26 19:13:58 -0300

    Terminado el ejercicio de invertir el numero

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,16 @@
+﻿int num;
+Console.WriteLine("Ingresar el numero que quiere invertir: ");
+num = Convert.ToInt32(Console.ReadLine());
+if (num < 10)
+{
+    Console.WriteLine("El numero invertido es: " + num);
+}
+else
+{
+    while (num >= 1)
+    {
+        int digito = num % 10;
+        num = num / 10;
+        Console.WriteLine(digito);
+    }
+}
